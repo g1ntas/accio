57a7dc0769c9fb9ba89a139b00c9b890f2234291
Gintas Kovalevskis <gintas@gintass-mbp.home>
2019-04-25 21:36:21 +0200

    Changed parser strategy && partial parsing implementation

--- markup/parser.go
+++ markup/parser.go
@@ -1,81 +1,155 @@
 package markup
 
-import "runtime"
+import (
+	"fmt"
+	"runtime"
+)
 
-// Tree is the representation of a single parsed template.
-type Tree struct {
-	Name string // name of the template represented by the tree.
-	ParseName string // name of the top-level template during parsing, for error messages.
-	Tags *[]Tag // list of tag nodes of the tree.
-	text string // text parsed to create the template.
-	// Parsing only; cleared after parse.
-	lex *lexer
-	token token // token currently being parsed.
+// Tag todo
+type Tag struct {
+	Attributes map[string]string
+	Body string
+	Name string
 }
 
-// Parse returns a parse.Tree of the template. If an error is encountered,
+// Pos represents a byte position in the original input text from which
+// this template was parsed.
+type Pos int
+
+func (p Pos) Position() Pos {
+	return p
+}
+
+// parser is the representation of a single parsed template.
+type parser struct {
+	Name string // name of the template represented by the tree.
+	Tags []*Tag // list of nodes of the tree.
+	text string // text parsed to create the template.
+	// For parsing only; cleared after parse.
+	lex *lexer
+	token token // token currently being parsed.
+	tag *Tag // tag currently being builded.
+}
+
+// Parse returns a parse.parser of the template. If an error is encountered,
 // parsing stops and an empty map is returned with error.
-func Parse(name, text, leftDelim, rightDelim string) (*Tree, error) {
-	t := New(name)
+func Parse(name, text, leftDelim, rightDelim string) (*parser, error) {
+	t := &parser{Name: name}
 	t.text = text
 	_, err := t.Parse(text, leftDelim, rightDelim)
 	return t, err
 }
 
-// New allocates a new parse tree with given name.
-func New(name string) *Tree {
-	return &Tree{Name: name}
-}
-
 // recover is the handler that turns panic into returns from the top level of Parse.
-func (t *Tree) recover(errp *error) {
+func (p *parser) recover(errp *error) {
 	e := recover()
 	if e != nil {
 		if _, ok := e.(runtime.Error); ok {
 			panic(e)
 		}
-		if t != nil {
-			t.lex.drain()
-			t.stopParse()
+		if p != nil {
+			p.lex.drain()
+			p.stop()
 		}
 		*errp = e.(error)
 	}
 }
 
-// stopParse terminates parsing.
-func (t *Tree) stopParse() {
-	t.lex = nil
+// stop terminates parser.
+func (p *parser) stop() {
+	p.lex = nil
 }
 
 // Parse parses the template definition string to construct a representation of
 // the template for execution. If either body delimiter string is empty, the
 // default ("<<" or ">>") is used.
-func (t *Tree) Parse(text, leftDelim, rightDelim string) (tree *Tree, err error) {
-	defer t.recover(&err)
-	t.ParseName = t.Name
-	t.Tags = &[]Tag{}
-	t.lex = lex(t.Name, text, leftDelim, rightDelim) // start parsing
-	t.text = text
-	t.parse()
-	t.stopParse()
-	return t, nil
+func (p *parser) Parse(text, leftDelim, rightDelim string) (tree *parser, err error) {
+	defer p.recover(&err)
+	p.Tags = []*Tag{}
+	p.lex = lex(p.Name, text, leftDelim, rightDelim) // start parsing
+	p.text = text
+	p.parseTemplate()
+	p.stop()
+	return p, nil
 }
 
-// parse is the top-level parser for a template. It runs to EOF.
-func (t *Tree) parse() {
+// parseTemplate is the top-level parser for a template. It runs to EOF.
+func (p *parser) parseTemplate() {
 	for {
-		switch t.next().typ {
+		switch token := p.next(); token.typ {
 		case tokenEOF:
-			return;
+			return
 		case tokenIdentifier:
-			// parse tag
-		
+			p.parseTag()
+			continue
+		case tokenError:
+			return // todo: error
+
 		}
 
 	}
 }
 
+// parseTag todo
+func (p *parser) parseTag() {
+	// todo: check reserved words; In this case 'delimiters': must be at the beginning of template.
+	p.newTag(p.token.val)
+	// consume next whitespace token
+	switch token := p.next(); token.typ {
+	case tokenEOF:
+	case tokenNewline:
+		return
+	case tokenSpace:
+		p.parseAttrOrBody()
+	default:
+		p.errorf("unexpected %s", token)
+	}
+}
+
+// parseAttrOrBody todo
+func (p *parser) parseAttrOrBody() {
+	switch token := p.next(); token.typ {
+	case tokenEOF:
+	case tokenNewline:
+		return
+	case tokenAttrDeclare:
+		p.parseAttr()
+	case tokenLeftDelim:
+		p.parseBody()
+	default:
+		p.errorf("unexpected %s", token)
+	}
+}
+
+// parseAttr todo
+func (p *parser) parseAttr() {
+	// todo
+}
+
+// parseBody todo
+func (p *parser) parseBody() {
+	// todo
+}
+
 // next returns the next token.
-func (t *Tree) next() token {
-	return t.lex.nextToken()
+func (p *parser) next() token {
+	return p.lex.nextToken()
+}
+
+// errorf formats the error and terminates processing.
+func (p *parser) errorf(format string, args ...interface{}) {
+	format = fmt.Sprintf("template at %s:%d: %s", p.Name, p.token.line, format)
+	panic(fmt.Errorf(format, args...))
+}
+
+// error terminates processing with error.
+func (p *parser) error(err error) {
+	p.errorf("%s", err)
+}
+
+// newTag todo
+func (p *parser) newTag(name string) *Tag {
+	p.tag = &Tag{Name: name}
+	p.Tags = append(p.Tags, p.tag)
+	return p.tag
 }
