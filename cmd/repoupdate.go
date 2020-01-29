c2f9061627d37d259aaeb2514b9504e0c1baaec2
Gintas Kovalevskis <3449458+g1ntas@users.noreply.github.com>
2020-01-29 16:41:14 +0100

    [wip] implement repo update command

--- cmd/repoupdate.go
+++ cmd/repoupdate.go
@@ -1,21 +1,55 @@
 package cmd
 
 import (
-	"fmt"
+	"errors"
+	"github.com/g1ntas/accio/generator"
 
 	"github.com/spf13/cobra"
 )
 
 // updateCmd represents `accio repo update` command
 var updateCmd = &cobra.Command{
-	Use:   "update",
-	Short: "Pull latest changes from added repositories",
+	Use:   "update [repository]",
+	Short: "Update repository with recent changes",
 	Long: ``,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("update called")
+	Args: cobra.MaximumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var repo *generator.Repository
+		if len(args) > 0 {
+			repo, _ = env.registry.Repos[args[0]]
+		}
+		if repo == nil {
+			var err error
+			repo, err = promptSelectRepo()
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
 	},
 }
 
 func init() {
+	updateCmd.Flags().Bool("all", false, "Update all repositories")
+	updateCmd.Flags().String("replace", "", "Replace specified repository with a new path/url")
+	updateCmd.Flags().BoolP("help", "h", false, "Show help")
 	repoCmd.AddCommand(updateCmd)
+}
+
+func promptSelectRepo() (*generator.Repository, error) {
+	if len(env.registry.Repos) == 0 {
+		return nil, errors.New("no repositories added yet")
+	}
+	choices, i := make([]string, len(env.registry.Repos)), 0
+	for _, r := range env.registry.Repos {
+		choices[i] = r.Path
+		i++
+	}
+	r, err := env.prompter.SelectOne("Choose repository:", "", choices)
+	if err != nil {
+		return nil, err
+	}
+	repo, _ := env.registry.Repos[r]
+	return repo, nil
 }
